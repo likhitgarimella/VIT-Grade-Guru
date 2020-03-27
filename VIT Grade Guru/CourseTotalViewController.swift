12c97b658b6dd6b815f29efc3160b26f406b07aa
Likhit <likhitgmella@gmail.com>
2020-03-28 02:16:16 +0530

    SwiftUI Code changes in course total

--- VIT Grade Guru/CourseTotalViewController.swift
+++ VIT Grade Guru/CourseTotalViewController.swift
@@ -30,179 +30,181 @@ class CourseTotalViewController: UIViewController {
     
     func Calculation() {
         
-        if (cat1.text!.isEmpty || cat2.text!.isEmpty || da1.text!.isEmpty || da2.text!.isEmpty || da3.text!.isEmpty || fat.text!.isEmpty) {
-            print("Empty Fields 1st Six")
-            /*
-            // Alert
-            let myAlert = UIAlertController(title: "Alert", message: "Empty Fields", preferredStyle: UIAlertController.Style.alert)
-            let okAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
-            myAlert.addAction(okAction)
-            self.present(myAlert, animated: true, completion: nil)
-            return
-            */      // Getting clashed with Popup
-        }   // If 1st 6 fields are empty
-            
-        else {
-            
+        let string1 = cat1.text!
+        var number1 = Double(string1.components(separatedBy: CharacterSet.decimalDigits.inverted).joined())
+        let string2 = cat2.text!
+        var number2 = Double(string2.components(separatedBy: CharacterSet.decimalDigits.inverted).joined())
+        let string3 = da1.text!
+        var number3 = Double(string3.components(separatedBy: CharacterSet.decimalDigits.inverted).joined())
+        let string4 = da2.text!
+        var number4 = Double(string4.components(separatedBy: CharacterSet.decimalDigits.inverted).joined())
+        let string5 = da3.text!
+        var number5 = Double(string5.components(separatedBy: CharacterSet.decimalDigits.inverted).joined())
+        let string6 = fat.text!
+        var number6 = Double(string6.components(separatedBy: CharacterSet.decimalDigits.inverted).joined())
+        let string7 = lab.text!
+        var number7 = Double(string7.components(separatedBy: CharacterSet.decimalDigits.inverted).joined())
+        let string8 = project.text!
+        var number8 = Double(string8.components(separatedBy: CharacterSet.decimalDigits.inverted).joined())
+        
+        // Textfield Validation
+        if (cat1.text!.isEmpty) {
+           number1 = 0
+        }
+        if (cat2.text!.isEmpty) {
+           number2 = 0
+        }
+        if (da1.text!.isEmpty) {
+           number3 = 0
+        }
+        if (da2.text!.isEmpty) {
+           number4 = 0
+        }
+        if (da3.text!.isEmpty) {
+           number5 = 0
+        }
+        if (fat.text!.isEmpty) {
+           number6 = 0
+        }
+        if (lab.text!.isEmpty) {
+           number7 = 0
+        }
+        if (project.text!.isEmpty) {
+           number8 = 0
+        }
+        
         if labSwitch.isOn==true && projectSwitch.isOn==true {
             if (lab.text!.isEmpty || project.text!.isEmpty) {
                 print("Empty Fields Last Two")
-                /*
-                // Alert
-                let myAlert = UIAlertController(title: "Alert", message: "Empty Fields", preferredStyle: UIAlertController.Style.alert)
-                let okAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
-                myAlert.addAction(okAction)
-                self.present(myAlert, animated: true, completion: nil)
-                return
-                */      // Getting clashed with Popup
-            }   // If Lab and Project Fields are empty
-            else {
-            answer.text = String( Double( (( ((3*(Double(cat1.text!)! + Double(cat2.text!)!))/10) + Double(da1.text!)! + Double(da2.text!)! + Double(da3.text!)! + (2*(Double(fat.text!)!))/5 )/2) ) + Double( (Double(lab.text!)!)/4 ) + Double( (Double(project.text!)!)/4 ) )
+                // If Lab and Project Fields are empty
+            } else {
+            answer.text = String( (( ((3*(number1!+number2!))/10) + number3! + number4! + number5! + ((2*number6!)/5) )/2) + (number7!)/4 + (number8!)/4)
             }   // If Lab and Project Fields aren't empty
         }   // If both Lab and Project Switches are On
         
         if labSwitch.isOn==true && projectSwitch.isOn==false {
             if (lab.text!.isEmpty) {
                 print("Empty Lab Field")
-                /*
-                // Alert
-                let myAlert = UIAlertController(title: "Alert", message: "Empty Lab Field", preferredStyle: UIAlertController.Style.alert)
-                let okAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
-                myAlert.addAction(okAction)
-                self.present(myAlert, animated: true, completion: nil)
-                return
-                */      // Getting clashed with Popup
-            }   // If Lab Field is empty
-            else {
-                answer.text = String( Double( (3*( ((3*(Double(cat1.text!)! + Double(cat2.text!)!))/10) + Double(da1.text!)! + Double(da2.text!)! + Double(da3.text!)! + (2*(Double(fat.text!)!))/5 )/4 ) ) + Double( (Double(lab.text!)!)/4 ) )
+                // If Lab Field is empty
+            } else {
+                answer.text = String( (3*( ((3*(number1!+number2!))/10) + number3! + number4! + number5! + ((2*number6!)/5) )/4) + (number7!)/4)
             }   // If Lab Field isn't empty
         }   // If Lab Switch is On and Project Switche is Off
-            
+        
         if labSwitch.isOn==false && projectSwitch.isOn==true {
             if (project.text!.isEmpty) {
                 print("Empty Project Field")
-                /*
-                // Alert
-                let myAlert = UIAlertController(title: "Alert", message: "Empty Project Field", preferredStyle: UIAlertController.Style.alert)
-                let okAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
-                myAlert.addAction(okAction)
-                self.present(myAlert, animated: true, completion: nil)
-                return
-                */      // Getting clashed with Popup
-            }   // If Project Field is empty
-            else {
-                answer.text = String( Double( (3*( ((3*(Double(cat1.text!)! + Double(cat2.text!)!))/10) + Double(da1.text!)! + Double(da2.text!)! + Double(da3.text!)! + (2*(Double(fat.text!)!))/5 )/4 ) ) + Double( (Double(project.text!)!)/4 ) )
+                // If Project Field is empty
+            } else {
+                answer.text = String( (3*( ((3*(number1!+number2!))/10) + number3! + number4! + number5! + ((2*number6!)/5) )/4) + (number8!)/4)
             }   // If Project Field isn't empty
         }   // If Lab Switch is Off and Project Switche is On
-            
-        if labSwitch.isOn==false && projectSwitch.isOn==false {
-            answer.text = String( Double( ( ((3*(Double(cat1.text!)! + Double(cat2.text!)!))/10) + Double(da1.text!)! + Double(da2.text!)! + Double(da3.text!)! + (2*(Double(fat.text!)!))/5 ) ) )
-        }   // If both Lab and Project Switches are Off
-            
-        }   // If 1st 6 fields aren't empty
         
-    }   // Entire Calculation Function
+        if labSwitch.isOn==false && projectSwitch.isOn==false {
+            answer.text = String( ((3*(number1!+number2!))/10) + number3! + number4! + number5! + ((2*number6!)/5) )
+        }       // If both Lab and Project Switches are Off
+    }           // Entire Calculation Function
     
     /* //////////////////////////////////////////////////////////////////////////////// */
     
     // Function For Autoset Validation For TextFields
     func Autoset() {
         
-        cat1.keyboardType = .decimalPad
+        cat1.keyboardType = .numberPad
         cat1.text = "\(self.cat1number)"
         cat1.addTarget(self, action: #selector(didChangeText), for: .allEditingEvents)
         
-        cat2.keyboardType = .decimalPad
+        cat2.keyboardType = .numberPad
         cat2.text = "\(self.cat2number)"
         cat2.addTarget(self, action: #selector(didChangeText), for: .allEditingEvents)
         
-        da1.keyboardType = .decimalPad
+        da1.keyboardType = .numberPad
         da1.text = "\(self.da1number)"
         da1.addTarget(self, action: #selector(didChangeText), for: .allEditingEvents)
         
-        da2.keyboardType = .decimalPad
+        da2.keyboardType = .numberPad
         da2.text = "\(self.da2number)"
         da2.addTarget(self, action: #selector(didChangeText), for: .allEditingEvents)
         
-        da3.keyboardType = .decimalPad
+        da3.keyboardType = .numberPad
         da3.text = "\(self.da3number)"
         da3.addTarget(self, action: #selector(didChangeText), for: .allEditingEvents)
         
-        fat.keyboardType = .decimalPad
+        fat.keyboardType = .numberPad
         fat.text = "\(self.fatnumber)"
         fat.addTarget(self, action: #selector(didChangeText), for: .allEditingEvents)
         
-        lab.keyboardType = .decimalPad
+        lab.keyboardType = .numberPad
         lab.text = "\(self.labnumber)"
         lab.addTarget(self, action: #selector(didChangeText), for: .allEditingEvents)
         
-        project.keyboardType = .decimalPad
+        project.keyboardType = .numberPad
         project.text = "\(self.projectnumber)"
         project.addTarget(self, action: #selector(didChangeText), for: .allEditingEvents)
         
     }
     
     // Autoset Validation For TextFields
-    var cat1number: Double = 0 {
+    var cat1number: Int = 0 {
         didSet {
-            if cat1number > 50.0 {
-                cat1number = 50.0
+            if cat1number > 50 {
+                cat1number = 50
                 self.cat1.text = "\(cat1number)"
             }
         }
     }
-    var cat2number: Double = 0 {
+    var cat2number: Int = 0 {
         didSet {
-            if cat2number > 50.0 {
-                cat2number = 50.0
+            if cat2number > 50 {
+                cat2number = 50
                 self.cat2.text = "\(cat2number)"
             }
         }
     }
-    var da1number: Double = 0 {
+    var da1number: Int = 0 {
         didSet {
-            if da1number > 10.0 {
-                da1number = 10.0
+            if da1number > 10 {
+                da1number = 10
                 self.da1.text = "\(da1number)"
             }
         }
     }
-    var da2number: Double = 0 {
+    var da2number: Int = 0 {
         didSet {
-            if da2number > 10.0 {
-                da2number = 10.0
+            if da2number > 10 {
+                da2number = 10
                 self.da2.text = "\(da2number)"
             }
         }
     }
-    var da3number: Double = 0 {
+    var da3number: Int = 0 {
         didSet {
-            if da3number > 10.0 {
-                da3number = 10.0
+            if da3number > 10 {
+                da3number = 10
                 self.da3.text = "\(da3number)"
             }
         }
     }
-    var fatnumber: Double = 0 {
+    var fatnumber: Int = 0 {
         didSet {
-            if fatnumber > 100.0 {
-                fatnumber = 100.0
+            if fatnumber > 100 {
+                fatnumber = 100
                 self.fat.text = "\(fatnumber)"
             }
         }
     }
-    var labnumber: Double = 0 {
+    var labnumber: Int = 0 {
         didSet {
-            if labnumber > 100.0 {
-                labnumber = 100.0
+            if labnumber > 100 {
+                labnumber = 100
                 self.lab.text = "\(labnumber)"
             }
         }
     }
-    var projectnumber: Double = 0 {
+    var projectnumber: Int = 0 {
         didSet {
-            if projectnumber > 100.0 {
-                projectnumber = 100.0
+            if projectnumber > 100 {
+                projectnumber = 100
                 self.project.text = "\(projectnumber)"
             }
         }
@@ -211,28 +213,28 @@ class CourseTotalViewController: UIViewController {
     // ObjC Function For Autoset Validation For TextFields
     @objc func didChangeText() {
         if let num1 = Double(self.cat1.text!) {
-            self.cat1number = num1
+            self.cat1number = Int(num1)
         }
         if let num2 = Double(self.cat2.text!) {
-            self.cat2number = num2
+            self.cat2number = Int(num2)
         }
         if let num3 = Double(self.da1.text!) {
-            self.da1number = num3
+            self.da1number = Int(num3)
         }
         if let num4 = Double(self.da2.text!) {
-            self.da2number = num4
+            self.da2number = Int(num4)
         }
         if let num5 = Double(self.da3.text!) {
-            self.da3number = num5
+            self.da3number = Int(num5)
         }
         if let num6 = Double(self.fat.text!) {
-            self.fatnumber = num6
+            self.fatnumber = Int(num6)
         }
         if let num7 = Double(self.lab.text!) {
-            self.labnumber = num7
+            self.labnumber = Int(num7)
         }
         if let num8 = Double(self.project.text!) {
-            self.projectnumber = num8
+            self.projectnumber = Int(num8)
         }
     }
     
@@ -254,6 +256,9 @@ class CourseTotalViewController: UIViewController {
         
         // Button Properties
         ButtonProp()
+        
+        // PopUp UIView Properties
+        PopUpProp()
 
     }
     
@@ -321,7 +326,7 @@ class CourseTotalViewController: UIViewController {
     func PopUpAnimation() {
         
         blurView.bounds = self.view.bounds
-        popUpView.bounds = CGRect(x: 0, y: 0, width: self.view.bounds.width*0.8, height: self.view.bounds.height*0.2)
+        popUpView.bounds = CGRect(x: 0, y: 0, width: self.view.bounds.width*0.68, height: self.view.bounds.height*0.2)
         
     }
     
@@ -375,6 +380,16 @@ class CourseTotalViewController: UIViewController {
     @IBOutlet var blurView: UIVisualEffectView!
     
     @IBOutlet var popUpView: UIView!
+    
+    @IBOutlet var okOutlet: UIButton!
+    
+    func PopUpProp() {
+        
+        popUpView.layer.cornerRadius = 16
+        okOutlet.layer.cornerRadius = 16
+        okOutlet.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        
+    }
     
     @IBAction func Ok(_ sender: UIButton) {
         
